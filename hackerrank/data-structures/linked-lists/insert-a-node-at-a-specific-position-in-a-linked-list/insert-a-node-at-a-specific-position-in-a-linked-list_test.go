64bf4744f5443bae8f269577ac1a3c6019499628
Huray-hub <panos.skiadas12@gmail.com>
2023-10-10 23:29:08 +0300

    HackerRank problem 'Insert a node at a specific position in a linked list '

---
+++ hackerrank/data-structures/linked-lists/insert-a-node-at-a-specific-position-in-a-linked-list/insert-a-node-at-a-specific-position-in-a-linked-list_test.go
@@ -0,0 +1,11 @@
+package insertanodeataspecificpositioninalinkedlist
+
+import (
+	"testing"
+
+	"github.com/Huray-hub/coding_challenges/hackerrank/data-structures/testhelper"
+)
+
+func TestInsertANodeAtASpecificPositionInALinkedlist(t *testing.T) {
+	testhelper.RunTestCases(t, main)
+}
