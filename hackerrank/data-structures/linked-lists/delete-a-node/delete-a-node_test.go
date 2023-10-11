cd405c5edb16d51547eee04d2252b936080ce936
Huray-hub <panos.skiadas12@gmail.com>
2023-10-11 18:11:57 +0300

    HackerRank problem 'Delete a node'

---
+++ hackerrank/data-structures/linked-lists/delete-a-node/delete-a-node_test.go
@@ -0,0 +1,11 @@
+package deleteanode
+
+import (
+	"testing"
+
+	"github.com/Huray-hub/coding_challenges/hackerrank/data-structures/testhelper"
+)
+
+func TestDeleteANode(t *testing.T) {
+	testhelper.RunTestCases(t, main)
+}
