a54c7a79af9d3df9cdb721a7a7841fcf5a4a56a3
Huray-hub <panos.skiadas12@gmail.com>
2023-10-12 12:33:09 +0300

    HackerRank problem 'Reverse a linked-list'

---
+++ hackerrank/data-structures/linked-lists/reverse-a-linked-list/reverse-a-linked-list_test.go
@@ -0,0 +1,11 @@
+package reversealinkedlist
+
+import (
+	"testing"
+
+	"github.com/Huray-hub/coding_challenges/hackerrank/data-structures/testhelper"
+)
+
+func TestReverseALinkedList(t *testing.T) {
+	testhelper.RunTestCases(t, main)
+}
