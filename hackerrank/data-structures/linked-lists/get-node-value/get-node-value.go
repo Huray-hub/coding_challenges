99c4bf8a8c21939c5a5722980744f728bf5c7f4b
Huray-hub <panos.skiadas12@gmail.com>
2023-10-14 20:41:11 +0300

    HackerRank problem 'Get Node Value'

---
+++ hackerrank/data-structures/linked-lists/get-node-value/get-node-value.go
@@ -0,0 +1,137 @@
+// Package getnodevalue contains solution for hackerrank problem https://www.hackerrank.com/challenges/get-the-value-of-the-node-at-a-specific-position-from-the-tail/problem
+package getnodevalue
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type SinglyLinkedListNode struct {
+	data int32
+	next *SinglyLinkedListNode
+}
+
+type SinglyLinkedList struct {
+	head *SinglyLinkedListNode
+	tail *SinglyLinkedListNode
+}
+
+func (singlyLinkedList *SinglyLinkedList) insertNodeIntoSinglyLinkedList(nodeData int32) {
+	node := &SinglyLinkedListNode{
+		next: nil,
+		data: nodeData,
+	}
+
+	if singlyLinkedList.head == nil {
+		singlyLinkedList.head = node
+	} else {
+		singlyLinkedList.tail.next = node
+	}
+
+	singlyLinkedList.tail = node
+}
+
+func printSinglyLinkedList(node *SinglyLinkedListNode, sep string, writer *bufio.Writer) {
+	for node != nil {
+		fmt.Fprintf(writer, "%d", node.data)
+
+		node = node.next
+
+		if node != nil {
+			fmt.Fprint(writer, sep)
+		}
+	}
+}
+
+/*
+ * Complete the 'getNode' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_SINGLY_LINKED_LIST llist
+ *  2. INTEGER positionFromTail
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     data int32
+ *     next *SinglyLinkedListNode
+ * }
+ *
+ */
+
+func getNode(llist *SinglyLinkedListNode, positionFromTail int32) int32 {
+	// Write your code here
+	length := 0
+	currentNode := llist
+	for currentNode != nil {
+		length++
+		currentNode = currentNode.next
+	}
+	positionFromHead := length - 1 - int(positionFromTail)
+	currentNode = llist
+	for i := 0; i < positionFromHead; i++ {
+		currentNode = currentNode.next
+	}
+	return currentNode.data
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	testsTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	tests := int32(testsTemp)
+
+	for testsItr := 0; testsItr < int(tests); testsItr++ {
+		llistCount, err := strconv.ParseInt(readLine(reader), 10, 64)
+		checkError(err)
+
+		llist := SinglyLinkedList{}
+		for i := 0; i < int(llistCount); i++ {
+			var llistItemTemp int64
+			llistItemTemp, err = strconv.ParseInt(readLine(reader), 10, 64)
+			checkError(err)
+			llistItem := int32(llistItemTemp)
+			llist.insertNodeIntoSinglyLinkedList(llistItem)
+		}
+
+		positionTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+		checkError(err)
+		position := int32(positionTemp)
+
+		result := getNode(llist.head, position)
+
+		fmt.Fprintf(writer, "%d\n", result)
+	}
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ hackerrank/data-structures/linked-lists/get-node-value/get-node-value_test.go
@@ -0,0 +1,11 @@
+package getnodevalue
+
+import (
+	"testing"
+
+	"github.com/Huray-hub/coding_challenges/hackerrank/data-structures/testhelper"
+)
+
+func TestGetNodeValue(t *testing.T) {
+	testhelper.RunTestCases(t, main)
+}
