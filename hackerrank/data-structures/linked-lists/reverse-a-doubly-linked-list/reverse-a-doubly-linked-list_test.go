c30f14aa5dde1fb563cc874526dff6cd8af1a66d
Huray-hub <panos.skiadas12@gmail.com>
2023-10-15 23:43:01 +0300

    HackerRank problem 'Reverse a doubly linked list'

---
+++ hackerrank/data-structures/linked-lists/reverse-a-doubly-linked-list/reverse-a-doubly-linked-list_test.go
@@ -0,0 +1,11 @@
+package reverseadoublylinkedlist
+
+import (
+	"testing"
+
+	"github.com/Huray-hub/coding_challenges/hackerrank/data-structures/testhelper"
+)
+
+func TestReverseADoublyLinkedList(t *testing.T) {
+	testhelper.RunTestCases(t, main)
+}
