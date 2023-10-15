e5d93636efe4cdd547729dd8c101b3437afeabf8
Huray-hub <panos.skiadas12@gmail.com>
2023-10-15 23:15:18 +0300

    HackerRank problem 'Delete duplicate-value nodes from a sorted linked list'

---
+++ hackerrank/data-structures/linked-lists/delete-duplicate-value-nodes-from-a-sorted-linked-list/delete-duplicate-value-nodes-from-a-sorted-linked-list_test.go
@@ -0,0 +1,11 @@
+package deleteduplicatevaluenodesfromasortedlinkedlist
+
+import (
+	"testing"
+
+	"github.com/Huray-hub/coding_challenges/hackerrank/data-structures/testhelper"
+)
+
+func TestDeleteDuplicateValueNodesFromASortedLinkedList(t *testing.T) {
+	testhelper.RunTestCases(t, main)
+}
